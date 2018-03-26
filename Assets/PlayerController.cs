5b963589630611e5c25096d302ecbcfb770ae2f0
kunitoku <kunitoku.git@gmail.com>
2018-03-26 16:07:54 +0900

    lesson06を修正

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private const float JumpForce = 700.0f;
     private const float WalkForce = 30.0f;
     private const float MaxWalkSpeed = 2.0f;
+    private const float Threshold = 0.2f;
 
     private void Start()
     {
@@ -14,27 +15,42 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        var key = 0;
+
+#if UNITY_EDITOR
         // ジャンプ
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _rigid2D.velocity.y == 0)
         {
             _rigid2D.AddForce(transform.up * JumpForce);
         }
 
         // 左右に移動
-        var key = 0;
         if (Input.GetKey(KeyCode.RightArrow)) key = 1;
         if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+#else
+        // ジャンプ
+        if (Input.GetMouseButtonDown(0) && _rigid2D.velocity.y == 0)
+        {
+            _rigid2D.AddForce(transform.up * JumpForce);
+        }
+
+        // 左右に移動
+        if (Input.acceleration.x > Threshold) key = 1;
+        if (Input.acceleration.x < -Threshold) key = -1;
+#endif
 
         // プレイヤーの速度
         var speedx = Mathf.Abs(_rigid2D.velocity.x);
 
         // スピード制限
-        if (speedx < MaxWalkSpeed) {
+        if (speedx < MaxWalkSpeed)
+        {
             _rigid2D.AddForce(transform.right * key * WalkForce);
         }
 
         // 動く方向に応じて反転
-        if (key != 0) {
+        if (key != 0)
+        {
             transform.localScale = new Vector3(key, 1, 1);
         }
     }
